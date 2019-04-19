5636fff4eec1342abe2183a56caaa99aa532f61c
Miguel <migueldisla28@gmail.com>
2019-04-19 12:16:40 -0400

    Borrando archivos VS y modificacion para el uso de arrow functions.

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+
+var app = express();
+
+const hello = (req,res) => res.send('Hello World!');
+
+const listener = () => console.log('Example app listening on port 3000!');
+
+app.get('/hello', hello);
+
+app.listen(3000, listener);
